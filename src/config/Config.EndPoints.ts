82ba60da952d602f151b84be5c28c0cff36a9405
Abel Alvarez <abel.a.alvarez@ericsson.com>
2023-10-31 18:17:17 -0600

    changes

--- src/config/Config.EndPoints.ts
+++ src/config/Config.EndPoints.ts
@@ -1,5 +1,5 @@
-const DOMAIN = 'http://localhost:4000';
-// const DOMAIN = 'https://worship-app-403017.wl.r.appspot.com/'
+// const DOMAIN = 'http://localhost:4000';
+const DOMAIN = 'https://worship-app-403017.wl.r.appspot.com/'
 export const LOGINROUTE = `${DOMAIN}/auth`
 export const USER_ROUTE =  `${DOMAIN}/users`
 export const CONTRIBUTION_ROUTE = `${DOMAIN}/contribution`
